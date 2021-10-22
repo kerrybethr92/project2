7d8b8831b0e0c5b013e3c53557c3dbc2178484ac
Kerry Beth Richardson <kerrybethrichardson@Kerrys-MacBook-Pro-2.local>
2021-10-21 19:24:35 -0500

    new user page

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
 const app = express();
 const db = mongoose.connection;
-require('dotenv').config()
+require('dotenv').config();
+const session = require('express-session');
 //___________________
 //Port
 //___________________
@@ -38,6 +39,13 @@ app.use(express.static('public'));
 
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
 app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false
+    })
+)
 app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
 
 //use method override
@@ -45,7 +53,12 @@ app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
 // Controllers
 const plantsController = require('./controllers/plants_controller.js')
-app.use('/plants', plantsController)
+const userController = require('./controllers/users_controller.js')
+// const sessionsController = require('./controllers/sessions_controller.js')
+app.use('/plants', plantsController);
+app.use('/users', userController);
+// app.use('/sessions', sessionsController);
+
 //___________________
 // Routes
 //___________________
